56ef7d7550dac286fe293b0abdf5b387518d7e10
DonnyLVu <donnyvu897@gmail.com>
2021-02-22 15:37:58 -0800

    add fixed blogSelector test

--- src/selectors/BlogSelector.test.js
+++ src/selectors/BlogSelector.test.js
@@ -3,9 +3,11 @@ import { getPosts } from './BlogSelector.js';
 describe('blog selectors', () => {
   it('Select Posts from state', () => {
     const state = {
-      posts: [{ title: 'Title 2', context: 'Context 2' }]
+      posts: {
+        posts: [{ title: 'Title 2', context: 'Context 2' }]
+      }
     };
-    console.log(state)
+    console.log(state);
     expect(getPosts(state)).toEqual(
       [{ title: 'Title 2', context: 'Context 2' }]);
   });
